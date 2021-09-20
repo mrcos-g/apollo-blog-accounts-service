f82fa5f8883857d00f0ed0368aef42dbd0db8353
mrcos-g <67390524+mrcos-g@users.noreply.github.com>
2021-09-19 21:14:19 -0400

    include updateAccount mutation

--- src/schema/mutations/updateAccount/updateAccount.ts
+++ src/schema/mutations/updateAccount/updateAccount.ts
@@ -7,15 +7,15 @@ import auth0 from '../../../config/auth0';
 
 import { User } from 'auth0';
 
+import requestToken from '../../../lib/getToken';
+
 @Resolver()
 export class UpdateAccountMutation {
-  @Mutation(() => String /*Account */, { nullable: true })
-  async updateAccount(
-    @Arg('input') input: UpdateAccountInput,
-  ): Promise<string> /*User */ {
+  @Mutation(() => Account, { nullable: true })
+  async updateAccount(@Arg('input') input: UpdateAccountInput): Promise<User> {
     const { email, id, password, newPassword } = input;
 
-    if (!email && newPassword && !password) {
+    if (!email && !newPassword && !password) {
       throw new UserInputError(
         'Cannot update account if you do not provide any information',
       );
@@ -23,15 +23,26 @@ export class UpdateAccountMutation {
       throw new UserInputError(
         'Email and password cannot be updated simultaneously',
       );
-    } else if ((!password && newPassword) || password) {
+    } else if ((!password && newPassword) || (password && !newPassword)) {
       throw new UserInputError(
         'You must provide your existing password, along with the new password, in order to update to a new password',
       );
     }
 
+    if (!email) {
+      const user = await auth0.getUser({ id });
+
+      await requestToken(user.email, password);
+      const updatedUser = await auth0.updateUser(
+        { id },
+        { password: newPassword },
+      );
+
+      return updatedUser;
+    }
+
     const user = await auth0.updateUser({ id }, {});
 
-    console.log('user is:', user);
-    return 'hello';
+    return user;
   }
 }
